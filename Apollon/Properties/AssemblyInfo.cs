caca4e66b0c742eea3eb1d70d0667975123f7437
Patrick Kranz <patrick-kranz@live.de>
2016-09-30 12:40:29 +0200

    Update to Aneversery Win10

--- Apollon/Properties/AssemblyInfo.cs
+++ Apollon/Properties/AssemblyInfo.cs
@@ -14,14 +14,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6ef9ca8e-b3ef-4f08-9dbf-275c6f09a997")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -34,3 +26,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
